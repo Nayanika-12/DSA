0d06e69853098208e512bde9dfe950c81d288f3d
Nayanika <nayanikasc2002@gmail.com>
2023-07-07 03:09:31 +0530

    Added

--- Minimized_largest_sum_of_the_list.cpp
+++ Minimized_largest_sum_of_the_list.cpp
@@ -19,4 +19,58 @@
 // Explanation: There are four ways to split nums into two subarrays.
 // The best way is to split it into [1,2,3] and [4,5], where the largest sum among the two subarrays is only 9.
 
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
 
+        int l=0,r=0,result,mid;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            l = max(l,nums[i]);
+            r += nums[i];
+        }
+
+        while(l <=r )
+        {
+            mid = (l+r)/2;
+            if(search(nums,k,mid))
+            {
+                result = mid;
+                r = mid-1;
+            }
+            else
+            {
+                l = mid+1;
+            }
+        }
+
+     return result;
+
+    }
+
+    bool search(vector<int>& nums, int k, int mid)
+    {
+        int sum=0,subarray=1,ans;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+           sum += nums[i];
+           if(sum > mid)
+           {
+               sum = nums[i];
+               subarray++;
+           }
+        }
+
+        if(subarray <= k)
+        {
+         ans = mid;
+         return true;
+        }
+        else
+        return false;
+
+    }
+
+};
